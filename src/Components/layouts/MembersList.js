6d50bf3d1c2860ca1c26f4054dd3644fe6495965
Sanket Landage <sanket.landage@gmail.com>
2021-08-12 20:09:57 +0530

    Family Member Add

---
+++ src/Components/layouts/MembersList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MembersList() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default MembersList
--- src/Components/pages/UserProfile.js
+++ src/Components/pages/UserProfile.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import AddMember  from '../layouts/AddMember';
+import MembersList  from '../layouts/MembersList';
+import { useEffect} from 'react';
+import { useLocation } from "react-router-dom";
 
 function UserProfile() {
+    const location = useLocation();
+
+    useEffect(() => {
+       console.log(location.pathname); // result: '/secondpage'
+       console.log(location.search); // result: '?query=abc'
+       console.log(location.state.detail); // result: 'some_value'
+    }, [location]);
+
+    const userData = location.state.detail;
     return (
-        <div>
-            <h1>User Profile</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-4 col-12">
+                    <AddMember userData={userData}/>
+                </div>
+                <div className="col-sm-8 col-12">
+                    <MembersList userData={userData}/>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default UserProfile
+export default UserProfile;
